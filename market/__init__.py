c4b86b524e4efcd31679457f7fdd66d61cf9fb8a
Ali Imam <mraliimam@gmail.com>
2024-12-31 20:48:36 +0500

    Logger updated

--- market/__init__.py
+++ market/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from datetime import datetime
 # from routes.main_routes import main_routes
 # from routes.file_routes import file_routes
 import logging
-from logging.handlers import RotatingFileHandler
+from logging.handlers import TimedRotatingFileHandler
 import os
 
 app = Flask(__name__)
@@ -18,11 +19,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 # Ensure upload directory exists
 os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
-# Set up logging
-if not os.path.exists('logs'):
-    os.makedirs('logs')
-
-file_handler = RotatingFileHandler('logs/audit_tool.log', maxBytes=10240, backupCount=10)
+log_dir = os.path.join(os.getcwd(), "logs")  # Use a directory within the current working directory
+# ... existing code ...
+if not os.path.exists(log_dir):
+    os.makedirs(log_dir)
+log_file = os.path.join(log_dir, datetime.now().strftime("%Y-%m-%d") + ".log")
+file_handler = TimedRotatingFileHandler(log_file, when="midnight", interval=1, backupCount=7)
+# file_handler = RotatingFileHandler('logs/audit_tool.log', maxBytes=10240, backupCount=10)
 file_handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(formatter)
